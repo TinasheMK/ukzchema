eb7ad56157d86ba429ec276c965606c9baad8a46
Tinashe Makarudze <tinashekmakarudze@gmail.com>
2023-02-12 17:20:35 +0200

    Update added.blade.php

--- resources/views/mail/obituary/added.blade.php
+++ resources/views/mail/obituary/added.blade.php
@@ -6,7 +6,7 @@ UKZ Chema Board Member has added an Obituary. Details:
 <p align="center">
     <img src="{{asset('storage/'.$obituary->photo)}}" height="84" alt="{{$obituary->full_name}}">
 </p>
-## Members are paying £{{$obituary->donated_amount}} excluding transaction charges
+## Members are paying £{{$obituary->donated_amount}} excluding Paypal transaction charges
 
 <br>**Total Amount For The Claim: £{{$obituary->goal_amount}}**
 <br>**Member ID:** {{$obituary->member_id}}
