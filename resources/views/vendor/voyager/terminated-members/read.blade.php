c9d6489747ba13b573eedd4118201236901663fd
Tinashe Makarudze <tinashekmakarudze@gmail.com>
2023-03-01 22:55:28 +0200

    Update read.blade.php

--- resources/views/vendor/voyager/terminated-members/read.blade.php
+++ resources/views/vendor/voyager/terminated-members/read.blade.php
@@ -44,19 +44,6 @@ $member = $dataTypeContent;
     @endcan
 
     @can('delete', $dataTypeContent)
-    @if($isSoftDeleted)
-    <a href="{{ route('voyager.'.$dataType->slug.'.restore', $dataTypeContent->getKey()) }}"
-        title="{{ __('voyager::generic.restore') }}" class="btn btn-default restore"
-        data-id="{{ $dataTypeContent->getKey() }}" id="restore-{{ $dataTypeContent->getKey() }}">
-        Restore
-        <i class="voyager-trash"></i> <span class="hidden-xs hidden-sm">{{ __('voyager::generic.restore') }}</span>
-    </a>
-    @else
-    <a href="javascript:;" title="Terminate" class="btn btn-danger delete" data-id="{{ $dataTypeContent->getKey() }}"
-        id="delete-{{ $dataTypeContent->getKey() }}">
-        <i class="voyager-trash"></i> <span class="hidden-xs hidden-sm">Terminate</span>
-    </a>
-    @endif
     @endcan
 </h1>
 
