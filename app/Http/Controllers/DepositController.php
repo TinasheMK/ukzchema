e7433a058c48b1376dadfc457d4fdbaa9d52108e
Tinashe Makarudze <73774313+TinasheMK@users.noreply.github.com>
2023-08-11 13:12:48 +0200

    Revert "deply revert"
    This reverts commit de57b5f2ac2749074cbbac3482f37a768e850336.

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -55,7 +55,7 @@ class DepositController extends SharedBaseController
             $dueDate = date("D M d, Y G:i", $date);
             $member->save();
             $this->payInvoices($user);
-            // Notification::route('mail', $member->email)->notify(new DepositNotification($amount));
+            Notification::route('mail', $member->email)->notify(new DepositNotification($amount));
 
         }
 
@@ -100,7 +100,7 @@ class DepositController extends SharedBaseController
             "description" => "Admin deposit for member"
         ]);
         $this->payInvoices($user);
-        // Notification::route('mail', $member->email)->notify(new DepositNotification($request->amount));
+        Notification::route('mail', $member->email)->notify(new DepositNotification($request->amount));
 
         $redirect = redirect()->route("voyager.members.index");
 
