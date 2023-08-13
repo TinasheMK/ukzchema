2c23004c6866e6f00dd28bb817066a85ce43b5a1
Tinashe Makarudze <73774313+TinasheMK@users.noreply.github.com>
2023-08-13 13:54:31 +0200

    dev

--- app/Notifications/AdminNotification.php
+++ app/Notifications/AdminNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class AdminNotification extends Notification
+class AdminNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
--- app/Notifications/ObituaryAddedNotification.php
+++ app/Notifications/ObituaryAddedNotification.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Auth;
 
-class ObituaryAddedNotification extends Notification
+class ObituaryAddedNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
