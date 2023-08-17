d22738681a360eff7500ed5e8d6c3260909e5a2b
Tinashe Makarudze <73774313+TinasheMK@users.noreply.github.com>
2023-08-17 21:25:55 +0200

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -83,7 +83,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@ukzchema.com'),
         'name' => env('MAIL_FROM_NAME', 'UKZ Board'),
     ],
 
