401b12c82fa011dd744ee5e84d0f0ff1da7b4a09
Tinashe Makarudze <73774313+TinasheMK@users.noreply.github.com>
2023-10-19 09:39:54 +0200

    dev

--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -2,29 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Member;
 use Illuminate\Http\Request;
 
 class AboutUsController extends Controller
 {
     public function index(){
-        $f_name =   '';
-        $l_name =   '';
-
-        $prefix = strtoupper($l_name . $f_name);
-        $prefix2 = "CM";
-
-        $counter = Member::count();
-        $padded = 0;
-        $member_id = null;
-
-        do {
-            $counter++;
-            $padded = str_pad($counter, 6, '0', STR_PAD_LEFT);
-            $member_id = $prefix2 . $padded;
-        } while (Member::find($member_id) != null);
-
-        dd($member_id);
         return view("pages.about");
     }
 }
