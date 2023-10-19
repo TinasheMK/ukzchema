18181bb540063ab5ad4b9f74002f271fc9611ca2
Tinashe Makarudze <73774313+TinasheMK@users.noreply.github.com>
2023-10-19 09:52:47 +0200

    Update AboutUsController.php

--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Member;
+// use App\Models\Member;
 use Illuminate\Http\Request;
 
 class AboutUsController extends Controller
 {
     public function index(){
-        dd(Member::withTrashed()->find("CM003381"));
+        // dd(Member::withTrashed()->find("CM003381"));
         return view("pages.about");
     }
 }
