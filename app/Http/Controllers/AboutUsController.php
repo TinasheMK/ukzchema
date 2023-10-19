8b8fdf144be443868e240577babd97e1ff47c0bc
Tinashe Makarudze <73774313+TinasheMK@users.noreply.github.com>
2023-10-19 09:44:49 +0200

    Update AboutUsController.php

--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
 use Illuminate\Http\Request;
 
 class AboutUsController extends Controller
 {
     public function index(){
+        dd(Member::find("CM003381"));
         return view("pages.about");
     }
 }
