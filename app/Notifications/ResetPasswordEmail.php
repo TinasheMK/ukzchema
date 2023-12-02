926206d9c6f2dbe9d0daaa1bf7ec41abd2b3702d
Tinashe Makarudze <73774313+TinasheMK@users.noreply.github.com>
2023-12-02 16:14:54 +0200

    Update ResetPasswordEmail.php

--- app/Notifications/ResetPasswordEmail.php
+++ app/Notifications/ResetPasswordEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ResetPasswordEmail extends Notification implements ShouldQueue
+class ResetPasswordEmail extends Notification
 {
     use Queueable;
 
